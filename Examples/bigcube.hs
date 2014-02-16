77a131c7ea00db3769c7ad9562abaebab3e1eae7
Michael D. Misamore <m.misamore@gmail.com>
2014-02-16 14:42:55 -0500

    Add examples

---
+++ Examples/bigcube.hs
@@ -0,0 +1,10 @@
+import Math.Topology.CubeCmplx.DirCubeCmplx
+import Math.Topology.CubeCmplx.CornerReduce
+import Control.DeepSeq
+
+cx = vsCmplx $ vsCoordsUnsafe (replicate 5 1) (replicate 5 6)
+
+main = print $ cx `deepseq` cmplxReducePar cx 
+         [vsVert $ vertexUnsafe (replicate 5 1),
+          vsVert $ vertexUnsafe (replicate 5 6)]
+
---
+++ Examples/smallcube.hs
@@ -0,0 +1,10 @@
+import Math.Topology.CubeCmplx.DirCubeCmplx
+import Math.Topology.CubeCmplx.CornerReduce
+import Control.DeepSeq
+
+cx = vsCmplx $ vsCoordsUnsafe (replicate 3 1) (replicate 3 10)
+
+main = print $ cx `deepseq` cmplxReducePar cx 
+         [vsVert $ vertexUnsafe (replicate 3 1),
+          vsVert $ vertexUnsafe (replicate 3 10)]
+
